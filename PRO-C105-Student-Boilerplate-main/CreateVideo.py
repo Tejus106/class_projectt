3a6a6dc7b408b841debad7492c8435f556bb8f12
Tejus106 <155088362+Tejus106@users.noreply.github.com>
2023-12-28 21:02:06 +0530

    Add files via upload

---
+++ PRO-C105-Student-Boilerplate-main/CreateVideo.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+
+
+path = "Images"
+
+images = []
+
+
+for file in os.listdir(path):
+    name, ext = os.path.splitext(file)
+
+    if ext in ['.gif', '.png', '.jpg', '.jpeg','.jfif']:
+        file_name = path+"/"+file
+
+        print(file_name)
+               
+        images.append(file_name)
+        
+print(len(images))
+count = len(images)
+frame=cv2.imread(images[0])
+height,widht,channels=frame.shape
+size=(widht,height)
+print(size)
+out=cv2.VideoWriter('project.mp4',cv2.VideoWriter_fourcc(*'DIVX')(5,size))
